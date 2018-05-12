f5ae866037f08075c64fa4e25a9e2b2353d49ee3
lovika <lovikasaxena@gmail.com>
2018-05-12 15:43:16 +0530

    1st commit

--- TicTacToe/src/main/java/GameMain.java
+++ TicTacToe/src/main/java/GameMain.java
@@ -1,7 +1,7 @@
 public class GameMain {
     public static void main(String[] args) {
         try {
-            String output = new TicTacToe(new Player('X'), new Player('O'), new Board(4)).play();
+            String output = new TicTacToe(new Player('X'), new Player('O'), new Board(3)).play();
             System.out.println(output);
         }
         catch (WrongInputException e){
